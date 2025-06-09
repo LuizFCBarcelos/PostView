82e57a51f2eb4040450020a7186771019adbe638
LuizFCBarcelos <luizfernandodecamposbarcelos@gmail.com>
2025-06-09 04:45:58 -0300

    feat/postlist: implementa requisição GET para buscar posts da API

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,5 +3,12 @@ import { useState, useEffect } from 'react'
 export default function PostList() {
   const [posts, setPosts] = useState([])
 
+    useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data))
+      .catch(error => console.error('Erro ao buscar posts:', error))
+  }, [])
+
   return <h1>Lista de Posts ({posts.length})</h1>
 }
